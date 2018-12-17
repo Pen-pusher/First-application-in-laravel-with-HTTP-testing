0bf7eb36780b2ed58e9bc078b43d600b82bb0a75
Piyush Chakraborty <piyushchakraborty9@gmail.com>
2018-12-17 18:03:44 +0530

    testing

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,10 +12,27 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function ()
+ {
+    $links = \App\Link::all();
+
+    return view('welcome', ['links' => $links]);
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/submit', function () {
+    return view('submit');
+});
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255',
+    ]);
+
+    $link = tap(new App\Link($data))->save();
+
+    return redirect('/');
+});
